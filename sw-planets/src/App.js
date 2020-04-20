70481d77bd54fbbd2f0acc4bc372164259c60b77
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2020-04-20 15:25:58 +0200

    HOC with Context Api

--- sw-planets/src/App.js
+++ sw-planets/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
-import RandomPlanet from './components/random-planet/random-planet';
 import ErrorBoundry from './components/error-boundry/error-boundry';
 import SwapiService from "./services/swapi-service";
+import DummySwapiService from "./services/dummy-swapi-service";
 import { PersonList, PlanetList, StarshipList } from './components/sw-components/item-list';
 import './App.css';
 import { PersonDetails, PlanetDetails, StarshipDetails } from './components/sw-components/details';
+import { SwapiServiceProvider } from './components/swapi-service-context/swapi-service-context';
 
 
 export default class App extends Component {
@@ -25,22 +26,19 @@ export default class App extends Component {
   };
 
   render() {
-
- 
-
-
-
     return (
       <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
-          <PersonDetails itemId={11} />
-          <PlanetDetails itemId={5} />
-          <StarshipDetails itemId={9} />
-          <PersonList />
-          <StarshipList />          
-          <PlanetList />        
-        </div>
+       <SwapiServiceProvider value={this.swapiService}>
+          <div className="stardb-app">
+            <Header />
+            <PersonDetails itemId={11} />
+            <PlanetDetails itemId={5} />
+            <StarshipDetails itemId={9} />
+            <PersonList />
+            <StarshipList />          
+            <PlanetList />        
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundry>
     );
   }
