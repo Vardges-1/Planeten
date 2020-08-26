4cd41cc1d96bfbb9fc75feeb31fb31a977c67a77
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2020-08-26 16:07:21 +0200

    mek

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,7 +1,7 @@
 export default class SwapiService {
 
   _apiBase = 'https://swapi.dev/api';
-  _imageBase = 'https://starwars-visualguide.com/assets/img';
+  _imageBase = 'https://starwars-visualguide.com/as sets/img';
 
 
   getResource = async (url) => {
